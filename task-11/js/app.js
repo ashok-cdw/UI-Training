8254873670161f7aed1434822245b0e1256f18de
ashok-cdw <ashok.natarajan@cdw.com>
2023-02-23 20:18:11 +0530

    Update app.js
    done error handling

--- task-11/js/app.js
+++ task-11/js/app.js
@@ -1,23 +1,38 @@
-let calculator = {
-    add: function (number1, number2) {
-        return number1 + number2;
-    },
-    subtract: function (number1, number2) {
-        return number1 - number2;
-    },
-    multiply: function (number1, number2) {
-        return number1 * number2;
-    },
-    divide: function (number1, number2) {
-        return number1 / number2;
-    },
-    modulo: function (number1, number2) {
-        return number1 % number2
-    }
-}
+// calculator object
 
-console.log("Addition : ", calculator.add(10, 20));
-console.log("Subtraction : ", calculator.subtract(10, 20));
-console.log("Multiplication : ", calculator.multiply(10, 20));
-console.log("Division : ", calculator.divide(10, 20));
-console.log("Modulo : ", calculator.modulo(10, 20));
+const calculator = {
+  add: function (number1, number2) {
+    return number1 + number2;
+  },
+  subtract: function (number1, number2) {
+    return number1 - number2;
+  },
+  multiply: function (number1, number2) {
+    return number1 * number2;
+  },
+  divide: function (number1, number2) {
+    return number1 / number2;
+  },
+  modulo: function (number1, number2) {
+    return number1 % number2;
+  },
+};
+
+let number1 = 10;
+let number2 = 20;
+
+number1 = parseInt(number1);
+number2 = parseInt(number2);
+
+// It checks for truthy values
+// If true  -> calculations are done
+// Else -> Prints the error message in console
+if (number1 && number2) {
+  console.log("Addition : ", calculator.add(number1, number2));
+  console.log("Subtraction : ", calculator.subtract(number1, number2));
+  console.log("Multiplication : ", calculator.multiply(number1, number2));
+  console.log("Division : ", calculator.divide(number1, number2));
+  console.log("Modulo : ", calculator.modulo(number1, number2));
+} else {
+  console.error("Input should be a number");
+}
