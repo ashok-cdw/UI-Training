4124775bb4d63ac2f73cd2992c570109b7f592d0
ashok-cdw <ashok.natarajan@cdw.com>
2023-02-23 20:24:44 +0530

    Update app.js
    1. added variable declaration to variables
    2.corrected mistakes in sort method
    3.changed variables
    4.added proper commenting to methods
    5.removed array and printed directly
    6.corrected mistakes in print date method

--- task-10/js/app.js
+++ task-10/js/app.js
@@ -13,92 +13,123 @@ console.log(2 == "2");
 
 // 1
 
-cricketPlayers = ["Ashok", "Harish", "Dinesh", "Maamathi", "Kavya", "Ranjith", "Charan", "Hareesh", "Mathi", "Thendral", "Pavithra"]
+let cricketPlayers = [
+  "Ashok",
+  "Harish",
+  "Dinesh",
+  "Maamathi",
+  "Kavya",
+  "Ranjith",
+  "Charan",
+  "Hareesh",
+  "Mathi",
+  "Thendral",
+  "Pavithra",
+];
 
 console.log(cricketPlayers);
 
 // 2
 
-console.log(cricketPlayers.shift())
+console.log(cricketPlayers.shift());
 
 // 3
 
-console.log(cricketPlayers.length)
+console.log(cricketPlayers.length);
 
 // 4
 
-cricketPlayers.push("Ashok")
-console.log(cricketPlayers)
+cricketPlayers.push("Ashok");
+console.log(cricketPlayers);
 
 // 5
 
-cricketPlayers.sort(function (a, b) {
-    return a.toLowerCase() - b.toLowerCase();
-});
+cricketPlayers.sort();
 console.log(cricketPlayers);
 
 // 6
 
-cricketPlayersWithJerseyNumber = cricketPlayers.map((value) => value + " - " + Math.round(Math.random() * 100))
+let cricketPlayersWithJerseyNumber = cricketPlayers.map(
+  (value) => value + " - " + Math.round(Math.random() * 100)
+);
 console.log(cricketPlayersWithJerseyNumber);
 
 // 7
 
-cricketPlayersUpperCase = cricketPlayersWithJerseyNumber.map(value => value.toUpperCase())
+let cricketPlayersUpperCase = cricketPlayers.map((value) =>
+  value.toUpperCase()
+);
 console.log(cricketPlayersUpperCase);
 
 // Section 3
 
 // 1 - Generate Numbers from 1 to 100
 
+/*
+@description - This function prints numbers in the given range
+@params - start,end
+*/
 function generateNumbers(start, end) {
-    let arr = []
-    for (let value = start; value <= end; value++) {
-        arr.push(value);
-    }
-    return arr;
+  for (let value = start; value <= end; value++) {
+    console.log(value);
+  }
 }
+generateNumbers(1, 100);
 
-let result = generateNumbers(1, 100)
-console.log(result)
 
 // 2 - Generate Date in Required Format
 
+/*
+@description - This function print date in format : DD/MM/YYYY
+@params - dateObject
+*/
 function generateDateInFormat(dateObject) {
-    let date = (dateObject.getDate() + 1) < 10 ? "0" + (dateObject.getDate() + 1) : dateObject.getDate() + 1;
-    let month = (dateObject.getMonth() + 1) < 10 ? "0" + (dateObject.getMonth() + 1) : dateObject.getMonth() + 1;
-    let year = dateObject.getFullYear();
-    return date + "/" + month + "/" + year;
+  return dateObject.toLocaleDateString();
 }
-
-result = generateDateInFormat(new Date());
+let result = generateDateInFormat(new Date());
 console.log(result);
 
+
 // 3 - Celsius to Fahrenheit
+
+/*
+@description - This function returns temperature from celcius to fahrenheit
+@params - celsius
+*/
 function celsiusToFahrenheit(celsius) {
-    return (celsius * 1.8) + 32;
+  return celsius * 1.8 + 32;
 }
 
-result = celsiusToFahrenheit(32)
+result = celsiusToFahrenheit(32);
 console.log(result);
 
 
 // 4 - Average of Given Numbers
+
+/*
+@description - This function returns the average of given list of numbers
+@params - arrayOfNumbers
+*/
 function averageOfNumbers(arrayOfNumbers) {
-    let sum = arrayOfNumbers.reduce(function (total, currentNumber) {
-        return total + currentNumber;
-    }, 0);
-    let average = sum / arrayOfNumbers.length;
-    return average;
+  let sum = arrayOfNumbers.reduce(function (total, currentNumber) {
+    return total + currentNumber;
+  }, 0);
+  let average = sum / arrayOfNumbers.length;
+  return average;
 }
 
 result = averageOfNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(result);
 
+
 // 5 - Reverse a given string
 
+/*
+@description - This function returns reversed string
+@params - stringToBeReversed
+*/
 function reverseString(stringToBeReversed) {
-    return stringToBeReversed.split("").reverse().join("");
+  return stringToBeReversed.split("").reverse().join("");
 }
 
 result = reverseString("Ashok");
