a9d4fbdfddc11a42862d9c4e24b1a3d3aaa492b2
ashok-cdw <ashok.natarajan@cdw.com>
2023-02-27 15:26:04 +0530

    changes done in calculator object

--- task-11/js/app.js
+++ task-11/js/app.js
@@ -21,13 +21,7 @@ const calculator = {
 let number1 = 10;
 let number2 = 20;
 
-number1 = parseInt(number1);
-number2 = parseInt(number2);
-
-// It checks for truthy values
-// If true  -> calculations are done
-// Else -> Prints the error message in console
-if (number1 && number2) {
+if (!(isNaN(number1) || isNaN(number2))) {
   console.log("Addition : ", calculator.add(number1, number2));
   console.log("Subtraction : ", calculator.subtract(number1, number2));
   console.log("Multiplication : ", calculator.multiply(number1, number2));
