c2b0b5541783226dbe276d43bfb75b4fea3bbfb7
ashok-cdw <125631918+ashok-cdw@users.noreply.github.com>
2023-03-06 01:15:24 +0530

    Removed Local Storage And Implemented Objects

--- task-12/js/app.js
+++ task-12/js/app.js
@@ -1,60 +1,60 @@
-// When page loaded, it checks for any previous data
-if (localStorage.getItem("shapeData")) {
-    let shapeData = JSON.parse(localStorage.getItem("shapeData"));
-    if (shapeData.pageNo == 0) {
-        document.getElementsByClassName("main-content")[0].classList.remove("main-content-1");
-        document.getElementsByClassName("main-content")[1].classList.add("main-content-2");
-        document.getElementsByClassName("main-content")[2].classList.add("main-content-3");
-        document.getElementsByClassName("shapes")[shapeData.shapeNo].getElementsByTagName("i")[0].classList.add("fa-solid", "fa-check");
-        document.getElementsByClassName("content-button")[0].classList.remove("hide");
-    }
-    else if (shapeData.pageNo == 1) {
-        document.getElementsByClassName("main-content")[0].classList.add("main-content-1");
-        document.getElementsByClassName("main-content")[1].classList.remove("main-content-2");
-        document.getElementsByClassName("main-content")[2].classList.add("main-content-3");
-        renderSidePage();
-    }
-    else {
-        document.getElementsByClassName("main-content")[0].classList.add("main-content-1");
-        document.getElementsByClassName("main-content")[1].classList.add("main-content-2");
-        document.getElementsByClassName("main-content")[2].classList.remove("main-content-3");
-        renderResultPage();
-    }
+// Shape Data
+const shapes = {
+    shape: "",
+    shapeNo: 0,
+    side: 0,
+};
+
+let circle = {
+    shapeId: "shape-1",
+    formulaName: "RADIUS",
+    formula0: "r",
+    formula1: "0.433 <sup>*</sup> s <sup>*</sup> s",
+    formula2: "2&pi;r",
+    result2: () => (Math.PI * (shapes.side ^ 2)).toFixed(2),
+    result3: () => (2 * Math.PI * shapes.side).toFixed(2),
 }
+let triangle = {
+    shapeId: "shape-2",
+    formulaName: "SIDE",
+    formula0: "s",
+    formula1: "&pi;r<sup>2</sup>",
+    formula2: "3 * s",
+    result2: () => (0.433 * shapes.side * shapes.side).toFixed(2),
+    result3: () => (3 * shapes.side).toFixed(2),
+}
+let square = {
+    shapeId: "shape-3",
+    formulaName: "SIDE",
+    formula0: "s",
+    formula1: "s <sup>*</sup> s",
+    formula2: "4 <sup>*</sup> s",
+    result2: () => (Math.PI * (shapes.side ^ 2)).toFixed(2),
+    result3: () => (2 * Math.PI * shapes.side).toFixed(2),
+}
+
 
 /*
 @description -  When some shape is clicked, it takes care of adding / removing tick icon on the shape
-                It also creates data in the local storage to store data about the shapes
+                It also store data about the shapes in object
 @params - event
 */
 function shapeClicked(event) {
-    if (localStorage.getItem("shapeData")) {
-        let shapeData = JSON.parse(localStorage.getItem("shapeData"));
-        if (event.target.getAttribute("data-no") != null && event.target.getAttribute("data-no") != shapeData.shapeNo) {
-            document.getElementsByClassName("shapes")[shapeData.shapeNo].getElementsByTagName("i")[0].classList.remove("fa-solid", "fa-check");
-            event.target.getElementsByTagName("i")[0].classList.add("fa-solid", "fa-check");
-            localStorage.setItem(
-                "shapeData",
-                JSON.stringify({
-                    shape: event.target.getAttribute("data-shape"),
-                    shapeNo: event.target.getAttribute("data-no"),
-                    pageNo: 0,
-                })
-            );
-        }
-    } else {
-        localStorage.setItem(
-            "shapeData",
-            JSON.stringify({
-                shape: event.target.getAttribute("data-shape"),
-                shapeNo: event.target.getAttribute("data-no"),
-                pageNo: 0,
-            })
-        );
-        event.target.getElementsByTagName("i")[0].classList.add("fa-solid", "fa-check");
+    console.log(event.target);
+    if (shapes.shape != "" && event.target.getAttribute("data-shape") != null && event.target.getAttribute("data-shape") != shapes.shape) {
+        document.getElementsByClassName("shapes")[shapes.shapeNo].querySelector("i").classList.remove("fa-solid", "fa-check");
+        event.target.querySelector("i").classList.add("fa-solid", "fa-check");
+        shapes.shape = event.target.getAttribute("data-shape");
+        shapes.shapeNo = event.target.getAttribute("data-no");
     }
-    document.getElementsByClassName("content-button")[0].classList.remove("hide");
+    else {
+        shapes.shape = event.target.getAttribute("data-shape");
+        shapes.shapeNo = event.target.getAttribute("data-no");
+        event.target.querySelector("i").classList.add("fa-solid", "fa-check");
+    }
+    document.querySelector(".content-button").classList.remove("hide");
 }
+
 
 // Adds click event listener to the shapes
 let chooseShapes = document.querySelectorAll(".shapes");
@@ -65,38 +65,22 @@ for (const shape of chooseShapes) {
 // Choose Shape Button Listener
 let chooseBtn = document.getElementById("choose-shape");
 chooseBtn.addEventListener("click", () => {
-    if (!localStorage.getItem("shapeData")) {
-        alert("No Shapes Selected");
-    }
-    else {
-        // Changing Values Present in Local Storage
-        let shapeData = JSON.parse(localStorage.getItem("shapeData"));
-        localStorage.setItem(
-            "shapeData",
-            JSON.stringify({
-                shape: shapeData.shape,
-                shapeNo: shapeData.shapeNo,
-                pageNo: 1,
-            })
-        );
-        // Hiding Page 1 and Displaying Page 2
-        document.getElementsByClassName("main-content")[0].classList.add("main-content-1");
-        document.getElementsByClassName("main-content")[1].classList.remove("main-content-2");
-        renderSidePage();
-    }
+    // Hiding Page 1 and Displaying Page 2
+    document.getElementsByClassName("main-content")[0].classList.add("main-content-1");
+    document.getElementsByClassName("main-content")[1].classList.remove("main-content-2");
+    renderSideInputPage();
 });
 
 /*
-@description -  Render Page 2 with Data's in Local Storage according to Shape Selected in Page 1
+@description -  Render Page 2 with Data's in Object according to Shape Selected in Page 1
 @params - null
 */
-function renderSidePage() {
-    let shapeData = JSON.parse(localStorage.getItem("shapeData"));
+function renderSideInputPage() {
     let sideTitle = document.querySelector(".side-details-title");
-    if (shapeData.shapeNo == 0) {
+    if (shapes.shapeNo == 0) {
         sideTitle.innerText = "2. Enter Radius";
     }
-    else if (shapeData.shapeNo == 1) {
+    else if (shapes.shapeNo == 1) {
         sideTitle.innerText = "2. Enter Side (Base & Height)";
     }
     else {
@@ -107,24 +91,14 @@ function renderSidePage() {
 // Calculate Button Listener
 let calculateBtn = document.getElementById("choose-calculate");
 calculateBtn.addEventListener("click", () => {
-    let inputBox = document.getElementsByClassName("size-input-box")[0];
+    let inputBox = document.querySelector(".size-input-box");
     let inputValue = inputBox.value;
     // Validating Input
     if (inputValue == "" || isNaN(inputValue)) {
         alert("Invalid Input");
     }
     else {
-        // Changing Values Present in Local Storage
-        let shapeData = JSON.parse(localStorage.getItem("shapeData"));
-        localStorage.setItem(
-            "shapeData",
-            JSON.stringify({
-                shape: shapeData.shape,
-                shapeNo: shapeData.shapeNo,
-                pageNo: 2,
-                side: inputValue,
-            })
-        );
+        shapes.side = inputValue;
         // Hiding Page 2 and Displaying Page 3
         document.getElementsByClassName("main-content")[1].classList.add("main-content-2");
         document.getElementsByClassName("main-content")[2].classList.remove("main-content-3");
@@ -133,104 +107,51 @@ calculateBtn.addEventListener("click", () => {
 });
 
 /*
-@description -  Stores Final Results in Local Storage
-@params - shapeData, formulaResult1, formulaResult2, formulaResult3
+@description -  Render Result with Data's in Object
+@params - shape
 */
-function setFinalResult(shapeData, formulaResult1, formulaResult2, formulaResult3) {
-    localStorage.setItem(
-        "shapeData",
-        JSON.stringify({
-            shape: shapeData.shape,
-            shapeNo: shapeData.shapeNo,
-            pageNo: shapeData.pageNo,
-            side: shapeData.side,
-            formulaResult1,
-            formulaResult2,
-            formulaResult3,
-        })
-    );
+function updateResultData(shape) {
+    console.log(shape);
+    let resultShape = document.querySelector(".result-shape .shapes");
+    let resultTitle = document.querySelector(".result-title h1");
+    let formulaNameDiv = document.querySelectorAll(".formula-name")[0];
+    let formula = document.querySelectorAll(".formula");
+    let formulaResult = document.querySelectorAll(".formula-result");
+    resultShape.classList.add(shape.shapeId);
+    resultTitle.innerText = shapes.shape;
+    formulaNameDiv.innerText = shape.formulaName;
+    formula[0].innerText = shape.formula0;
+    formula[1].innerHTML = shape.formula1;
+    formula[2].innerHTML = shape.formula2;
 
+    formulaResult[0].innerText = shapes.side + " cm";
+    formulaResult[1].innerText = shape.result2() + " sq cm";
+    formulaResult[2].innerText = shape.result3() + "cm";
 }
 
+
 /*
-@description -  Render Page 3 with Data's in Local Storage
+@description -  Choose Shape with Data's in Object
 @params - null
 */
 function renderResultPage() {
-    let shapeData = JSON.parse(localStorage.getItem("shapeData"));
-    let resultShape = document.querySelector(".result-shape .shapes");
-    let resultTitle = document.querySelector(".result-title h1");
-    let formulaName = document.querySelectorAll(".formula-name")[0];
-    let formula = document.querySelectorAll(".formula");
-    let formulaResult = document.querySelectorAll(".formula-result");
 
     // Dynamic Rendering For Shape Circle
-    if (shapeData.shapeNo == 0) {
-        resultShape.classList.add("shape-1");
-        resultShape.classList.remove("shape-2");
-        resultShape.classList.remove("shape-3");
-        resultTitle.innerText = shapeData.shape;
-        formulaName.innerText = "RADIUS";
-        formula[0].innerText = "r";
-        formula[1].innerHTML = "&pi;r<sup>2</sup>";
-        formula[2].innerHTML = "2&pi;r";
-
-        formulaResult[0].innerText = shapeData.side + " cm";
-        formulaResult[1].innerText = (Math.PI * (shapeData.side ^ 2)).toFixed(2) + " sq cm";
-        formulaResult[2].innerText = (2 * Math.PI * shapeData.side).toFixed(2) + "cm";
-
-        setFinalResult(shapeData, shapeData.side, (Math.PI * (shapeData.side ^ 2)).toFixed(2), (2 * Math.PI * shapeData.side).toFixed(2));
-
+    if (shapes.shapeNo == 0) {
+        updateResultData(circle);
     }
     // Dynamic Rendering For Shape Triangle
-    else if (shapeData.shapeNo == 1) {
-        resultShape.classList.remove("shape-1");
-        resultShape.classList.remove("shape-3");
-        resultShape.classList.add("shape-2");
-        resultTitle.innerText = shapeData.shape;
-        formulaName.innerText = "SIDE";
-        formula[0].innerText = "s";
-        formula[1].innerHTML = 0.433 + " <sup>*</sup> s <sup>*</sup> s";
-        formula[2].innerHTML = "3 * s";
-
-        formulaResult[0].innerText = shapeData.side + " cm";
-        formulaResult[1].innerText = (0.433 * shapeData.side * shapeData.side).toFixed(2) + " sq cm";
-        formulaResult[2].innerText = (3 * shapeData.side).toFixed(2) + "cm";
-
-        setFinalResult(shapeData, shapeData.side, (0.433 * shapeData.side * shapeData.side).toFixed(2), (3 * shapeData.side).toFixed(2));
-
+    else if (shapes.shapeNo == 1) {
+        updateResultData(triangle);
     }
     // Dynamic Rendering For Shape Square
     else {
-        resultShape.classList.remove("shape-1");
-        resultShape.classList.remove("shape-2");
-        resultShape.classList.add("shape-3");
-        resultTitle.innerText = shapeData.shape;
-        formulaName.innerText = "SIDE";
-        formula[0].innerText = "s";
-        formula[1].innerHTML = "s <sup>*</sup> s";
-        formula[2].innerHTML = "4 <sup>*</sup> s";
-
-        formulaResult[0].innerText = shapeData.side + " cm";
-        formulaResult[1].innerText = (shapeData.side * shapeData.side).toFixed(2) + " sq cm";
-        formulaResult[2].innerText = (4 * shapeData.side).toFixed(2) + "cm";
-
-        setFinalResult(shapeData, shapeData.side, (shapeData.side * shapeData.side).toFixed(2), (4 * shapeData.side).toFixed(2));
+        updateResultData(square);
     }
 }
 
 // Try Again Button Listener
-let chooseTry = document.getElementById("choose-try");
+let chooseTry = document.querySelector("#choose-try");
 chooseTry.addEventListener("click", () => {
-    // Resetting Everything
-    localStorage.removeItem("shapeData");
-    let tickIcon = document.querySelectorAll(".shapes i");
-    for (const tick of tickIcon) {
-        tick.classList.remove("fa-solid", "fa-check");
-    }
-    // Hiding Page 3 and Displaying Page 1
-    document.getElementsByClassName("main-content")[2].classList.add("main-content-3");
-    document.getElementsByClassName("main-content")[0].classList.remove("main-content-1");
-    document.getElementsByClassName("content-button")[0].classList.add("hide");
-    document.getElementsByClassName("size-input-box")[0].value = "";
+    location.reload();
 })
